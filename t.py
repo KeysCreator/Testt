b8bdec4b95a4d9135d1b38310b7e05e67f82f76a
KeysCreator <143636898+KeysCreator@users.noreply.github.com>
2023-12-02 13:16:12 +0000

    Y

--- t.py
+++ t.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from flask import send_file
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return send_file("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True, port=3333)
